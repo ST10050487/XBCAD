20a8ba8cef1f9eb12784d03c3d01d62d7b1cbf42
ST10158643 <ST10158643@vcconnect.edu.za>
2024-10-28 14:25:41 +0200

    new admin home

---
+++ app/src/main/java/za/co/varsitycollage/st10050487/knights/AdminHome.kt
@@ -0,0 +1,20 @@
+package za.co.varsitycollage.st10050487.knights
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AdminHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_admin_home2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
