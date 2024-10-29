70dc28af115825f01f79c1a61e9687f7ad958510
ST100535561 <ST10053561@vcconnect.edu.za>
2024-10-29 15:48:33 +0200

    fixed the filter

--- app/src/main/java/za/co/varsitycollage/st10050487/knights/AdminSportsFixtures.kt
+++ app/src/main/java/za/co/varsitycollage/st10050487/knights/AdminSportsFixtures.kt
@@ -1,8 +1,11 @@
 package za.co.varsitycollage.st10050487.knights
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
 
 class AdminSportsFixtures : AppCompatActivity() {
 
@@ -16,6 +19,21 @@ class AdminSportsFixtures : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, fragment)
                 .commit()
+        }
+
+        // Set up the filter dialog
+        val filterIcon = findViewById<ImageView>(R.id.filter_icon)
+        filterIcon.setOnClickListener {
+            // Create and display the filter dialog
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.fixture_filter_popup) // Make sure filter_popup.xml is in res/layout
+            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            // Close icon in the dialog
+            val closeIcon = dialog.findViewById<ImageView>(R.id.close_icon)
+            closeIcon.setOnClickListener { dialog.dismiss() }
+
+            dialog.show()
         }
     }
 }
