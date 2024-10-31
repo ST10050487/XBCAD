ff51ad451d35b40840c0d1786966d4c0516dcede
ST10158643 <ST10158643@vcconnect.edu.za>
2024-10-31 23:42:35 +0200

    last push

--- app/src/main/java/za/co/varsitycollage/st10050487/knights/Login.kt
+++ app/src/main/java/za/co/varsitycollage/st10050487/knights/Login.kt
@@ -138,7 +138,7 @@ class Login : AppCompatActivity() {
                 loginAttempts = 0 // Reset attempts on successful login
                 val intent = when (roleId) {
                     1 -> Intent(this, AdminHome::class.java)
-                    2, 3 -> Intent(this, EventManagement::class.java)
+                    2, 3 -> Intent(this, HomeActivity::class.java)
                     else -> null
                 }
 
